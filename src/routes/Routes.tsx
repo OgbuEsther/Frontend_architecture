414438e00006d19b3e5673d68a2cc402dd75c93b
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-03 15:49:31 +0100

    added error boundary

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import {createBrowserRouter} from "react-router-dom"
+import { HomeLayout } from '../components'
+import { Home } from '../pages'
+import { ErrorBoundary } from '../utils'
 
 const Routes = () => {
+
+    const elememts = createBrowserRouter([
+        {
+            path : "/",
+            element : <HomeLayout />,
+            children : [
+                {
+                    index : true,
+                    element : <Home />,
+                    errorElement : <ErrorBoundary />,
+                    hasErrorBoundary : true
+                }
+            ]
+        }
+    ])
+
   return (
     <div>Routes</div>
   )
