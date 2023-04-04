dab4cc910a77c7c4c5b8460e2e11446f8fbcc55b
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-04 07:52:44 +0100

    created my pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    <div
+    style={{
+      backgroundColor: "#1234",
+      width: "100%",
+      height: "50vh",
+      color: "white",
+      fontSize: "30px",
+      fontWeight: "bold",
+    }}
+  >
+    this is about
+  </div>
+  
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+ 
+      <div
+        style={{
+          backgroundColor: "purple",
+          width: "100%",
+          height: "50vh",
+          color: "white",
+          fontSize: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        this is home
+      </div>
+
+  );
+};
+
+export default Home;
