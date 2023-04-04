676a999c3d54ac8ccaf25e39a0a70f53abf5a0c9
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-04 08:43:19 +0100

    structure done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
 const About = () => {
-    <div
-    style={{
-      backgroundColor: "#1234",
-      width: "100%",
-      height: "50vh",
-      color: "white",
-      fontSize: "30px",
-      fontWeight: "bold",
-    }}
-  >
-    this is about
-  </div>
+    return (
+        <div
+        style={{
+          backgroundColor: "#1234",
+          width: "100%",
+          height: "50vh",
+          color: "white",
+          fontSize: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        this is about
+      </div>
+    )
+   
   
 }
 
--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,11 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomeLayout } from "../components";
+import { About, Home } from "../pages";
+import { ErrorBoundary, NotFound } from "../utils";
 
 
 
 export const Elements = createBrowserRouter([
     {
         path : "/",
-        element : <HomeLayout />
+        element : <HomeLayout />,
+        children : [
+            {
+                index : true,
+                element : <Home />,
+                errorElement : <ErrorBoundary />,
+                hasErrorBoundary : true
+
+            },
+            {
+                path : "/about",
+                element : <About />,
+                errorElement: <ErrorBoundary />,
+                hasErrorBoundary : true
+
+            }
+        ]
+
+    },
+    {
+        path : "*",
+        element : <NotFound />
     }
 ])
